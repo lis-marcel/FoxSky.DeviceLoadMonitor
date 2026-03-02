91d02ecb7a5e0495e02e157d6113090d69fb56f1
Marcel Lis <lis.marcys@gmail.com>
2026-03-02 09:58:11 +0100

    [feat] added initial slave receiving code

--- src/FoxSky.DeviceLoadMonitor.Slave/src/main.cpp
+++ src/FoxSky.DeviceLoadMonitor.Slave/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED pin as an output
+  pinMode(GPIO26, OUTPUT); 
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);                       
 }
