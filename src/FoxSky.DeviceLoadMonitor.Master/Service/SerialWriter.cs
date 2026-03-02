fa05156d199b29f09aa31401a29669d75efb7ac1
marcel <lis.marcys@gmail.com>
2026-03-02 20:37:19 +0100

    [feat] fixed logging on master side

--- src/FoxSky.DeviceLoadMonitor.Master/Service/SerialWriter.cs
+++ src/FoxSky.DeviceLoadMonitor.Master/Service/SerialWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Ports;
+using System.Reflection;
 
 namespace FoxSky.DeviceLoadMonitor.Master.Service
 {
@@ -9,6 +10,7 @@ namespace FoxSky.DeviceLoadMonitor.Master.Service
         public SerialWriter()
         {
             _serialPort = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(WriteReceivedData);
             _serialPort.Open();
         }
 
@@ -19,6 +21,7 @@ namespace FoxSky.DeviceLoadMonitor.Master.Service
                 if (_serialPort.IsOpen)
                 {
                     _serialPort.Write(data);
+
                 }
                 else
                 {
@@ -30,6 +33,26 @@ namespace FoxSky.DeviceLoadMonitor.Master.Service
                 Console.WriteLine(ex);
             }
             
+        }
+
+        public void WriteReceivedData(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (_serialPort.IsOpen)
+                {
+                    string data = _serialPort.ReadExisting();
+                    Console.WriteLine($"{data}");
+                }
+                else
+                {
+                    throw new InvalidOperationException("Serial port is not open.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
